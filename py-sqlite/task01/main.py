c4952c2f2295fa4ff57eb3b3bcb257650cb951e4
sTIMUL <119098451+uncle-stimul@users.noreply.github.com>
2025-03-07 21:23:36 +0500

    Update main.py

--- py-sqlite/task01/main.py
+++ py-sqlite/task01/main.py
@@ -2,7 +2,4 @@ from menu import Menu
 
     
 if __name__ == "__main__":
-    try:
-        Menu.cli()
-    except KeyboardInterrupt:
-        print("\nВыполнение программы завершено, использовано сочетание клавиш [CTRL+C]")
+    Menu.cli()
