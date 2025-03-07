6d879a5bfc1b3ed415b47a8364883c5f2ef9b7ab
sTIMUL <119098451+uncle-stimul@users.noreply.github.com>
2025-03-07 21:24:49 +0500

    Update menu.py

--- py-sqlite/task01/menu.py
+++ py-sqlite/task01/menu.py
@@ -3,46 +3,162 @@ import os
 
 
 class Menu:
-    cli_lines = '''
-----------------------------------------
-Управление БД "Фонотека"
-----------------------------------------
-1) Переинициализировать БД
-2) Заполнить БД синтетическими данными
-3) Добавить новый объект
-4) Удалить существующий объект
-5) Обновить существующий объект
-6) Поиск по полям
-0) Выход из интерфейса управления
-----------------------------------------
-'''
+    def get_size() -> str:
+        return '─'*list(os.get_terminal_size())[0]
+
+    def err_input() -> None:
+        os.system('clear')
+        msg = f'{Menu.get_size()}\n[Ошибка]: Введен не корректный код действия, выберите код из списка'
+        print(msg)
+
+    def validate(obj: Repository.Object) -> bool:
+        if type(obj.album) == str:
+            print('[Ошибка] Для поля "названия альбома" передан не корректный тип данных')
+            return False
+        if type(obj.album) == str:
+            print('[Ошибка] Для поля "названия альбома" передан не корректный тип данных')
+            return False
+        return True
+
+    def show() -> None:
+        os.system('clear')
+        print(Menu.get_size() + '\nСоздание выборки из БД\n' + Menu.get_size())
+        Repository.search('album', '%')
+
+    def create() -> None:
+        os.system('clear')
+        menu = (
+            Menu.get_size()
+            + f'Добавление нового объекта в БД\n'
+            + Menu.get_size()
+            + f'\n1) Добавить новый альбом'
+            + f'\n2) Добавить нового исполнителя'
+            + f'\n3) Добавить новый жанр'
+            + f'\n4) Добавить новый год релиза'
+            + f'\n0) Вернуться к прошлому меню\n' 
+            + Menu.get_size()
+        )
+        obj = Repository.Object()
+        while True:
+            match input(f'{menu}\nВыберите действие: '):
+                case '0': 
+                    os.system('clear')
+                    return
+                case '1':
+                    obj.album = input(f'{Menu.get_size()}\nВведите название альбома: ')
+                    if Repository.exists(table='Albums', attr='album', value=obj.album):
+                        os.system('clear')
+                        print(f'{Menu.get_size()}\n[Ошибка] Объект с таким именем уже существует')
+                        return
+                    obj.musician = Repository.get_id(
+                        table='Musicians',
+                        attr='musician',
+                        value=input('Введите исполнителя альбома: ')
+                    )
+                    obj.style = Repository.get_id(
+                        table='Styles',
+                        attr='style',
+                        value=input('Введите жанр альбома: ')
+                    )
+                    obj.release = Repository.get_id(
+                        table='Releases',
+                        attr='year',
+                        value=input('Введите год релиза альбома: ')
+                    )
+                    table='Albums'
+                    break
+                case '2':
+                    obj.album = input(f'{Menu.get_size()}\nВведите имя исполнителя: ')
+                    table='Musicians'
+                    if Repository.exists(table=table, attr='musician', value=obj.album):
+                        os.system('clear')
+                        print(f'{Menu.get_size()}\n[Ошибка] Объект с таким именем уже существует')
+                        return
+                    break
+                case '3': 
+                    obj.album = input(f'{Menu.get_size()}\nВведите название жанра: ')
+                    table='Styles'
+                    if Repository.exists(table=table, attr='style', value=obj.album):
+                        os.system('clear')
+                        print(f'{Menu.get_size()}\n[Ошибка] Объект с таким именем уже существует')
+                        return
+                    break
+                case '4': 
+                    obj.album = input(f'{Menu.get_size()}\nВведите год релиза: ')
+                    table='Releases'
+                    if Repository.exists(table=table, attr='releases', value=obj.album):
+                        os.system('clear')
+                        print(f'{Menu.get_size()}\n[Ошибка] Объект с таким именем уже существует')
+                        return
+                    break
+                case _: Menu.err_input()
+        Repository.create(table, obj)
+
+    def delete() -> None:
+        os.system('clear')
+        menu = (
+            Menu.get_size()
+            + f'Удаление объекта из таблицы\n'
+            + Menu.get_size()
+            + f'\n1) Удалить один альбом'
+            + f'\n2) Удалить исполнителя и все связанные альбомы'
+            + f'\n3) Удалить жанр и все связанные альбомы'
+            + f'\n4) Удалить год релиза и все связанные альбомы'
+            + f'\n0) Вернуться к прошлому меню\n' 
+            + Menu.get_size()
+        )
+        while True:
+            match input(f'{menu}\nВыберите действие: '):
+                case '0': return
+                case '1':
+                    table = 'Albums'
+                    attr = 'album'
+                    value_name = 'наименование альбома'
+                    break
+                case '2':
+                    table = 'Musicians'
+                    attr = 'musician'
+                    value_name = 'имя исполнителя'
+                    break
+                case '3': 
+                    table = 'Styles'
+                    attr = 'style'
+                    value_name = 'наименование жанра'
+                    break
+                case '4': 
+                    table = 'Release'
+                    attr = 'year'
+                    value_name = 'год релиза'
+                    break
+                case _: Menu.err_input()
+        value = input(f'Введите {value_name}: ')
+        Repository.delete(table, attr, value)
+
     def cli() -> None:
         Repository.init()
-        err_input = ''
-        while True:
-            if err_input:
-                print(err_input)
-                err_input = ''
-            print(Menu.cli_lines)
-            match input('Выберите действие: '):
-                case '0':
-                    break
-                case '1':
-                    Repository.reinit()
-                case '2':
-                    Repository.sint()
-                case '3':
-                    new_obj = Repository.Object()
-                    new_obj.album = input('Введите название альбома: ')
-                    new_obj.musician = input('Введите исполнителя альбома: ')
-                    #проверка наличие объекта в таблице
-                    new_obj.style = input('Введите жанр альбома:')
-                    new_obj.release = input('Введите жанр альбома:')
-                    Repository.create(new_obj)
-                case '4':
-                    Repository.delete('123')
-                case '6':
-                    Repository.search('album', '%')
-                case _:
-                    os.system('clear')
-                    err_input = '----------------------------------------\nВведен не корректный код действия, выберите код из списка'
+        menu = (
+            Menu.get_size()
+            + '\nУправление БД [Фонотека]\n'
+            + Menu.get_size()
+            + '\n1) Переинициализировать БД'
+            + '\n2) Заполнить БД синтетическими данными'
+            + '\n3) Создание выборки по полям'
+            + '\n4) Добавить новый объект'
+            + '\n5) Обновить существующий объект'
+            + '\n6) Удалить существующий объект'
+            + '\n0) Выход из интерфейса управления\n'
+            + Menu.get_size()
+        )
+        try:
+            while True:
+                match input(f'{menu}\nВыберите действие: '):
+                    case '0': break
+                    case '1': Repository.reinit()
+                    case '2': Repository.sint()
+                    case '3': Menu.show()
+                    case '4': Menu.create()
+                    case '5': Menu.update()
+                    case '6': Menu.delete()
+                    case _: Menu.err_input()
+        except KeyboardInterrupt:
+            print(f"\n{Menu.get_size()}\nВыполнение программы завершено, использовано сочетание клавиш [CTRL+C]")
