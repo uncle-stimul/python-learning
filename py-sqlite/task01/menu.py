353f58d7422fad1ec6521a6b7cae12304c118b7f
sTIMUL <119098451+uncle-stimul@users.noreply.github.com>
2025-03-18 23:28:52 +0500

    Update menu.py

--- py-sqlite/task01/menu.py
+++ py-sqlite/task01/menu.py
@@ -22,8 +22,68 @@ class Menu:
 
     def show() -> None:
         os.system('clear')
-        print(Menu.get_size() + '\nСоздание выборки из БД\n' + Menu.get_size())
-        Repository.search('album', '%')
+        menu = (
+            Menu.get_size()
+            + '\nСоздание выборки из БД\n'
+            + Menu.get_size()
+            + '\n1) Вывести все значения из БД'
+            + '\n2) Вывести значения из БД с фильтрацией по названию альбома'
+            + '\n3) Вывести значения из БД с фильтрацией по исполнителю'
+            + '\n4) Вывести значения из БД с фильтрацией по жанру'
+            + '\n5) Вывести значения из БД с фильтрацией по году релиза'
+            + '\n0) Вернуться к прошлому меню\n'
+        )
+        while True:
+            match input(f'{menu}\nВыберите действие: '):
+                case '0': 
+                    os.system('clear')
+                    return
+                case '1':
+                    attr = 'album'
+                    value = '%'
+                    break
+                case '2':
+                    data = input(Menu.get_size() + '\nВыберите названия альбома для фильтрации: ')
+                    if Repository.exists('Albums', 'album', data):
+                        attr = 'album'
+                        value = data
+                        break
+                    else:
+                        os.system('clear')
+                        print(Menu.get_size() + 'Ошибка: введенно не существующие значение')
+                case '3':
+                    data = input(Menu.get_size() + '\nВыберите исполнителя для фильтрации: ')
+                    if Repository.exists('Musicians', 'musician', data):
+                        attr = 'musician'
+                        value = Repository.get_id('Musicians', 'musician', data)
+                        break
+                    else:
+                        os.system('clear')
+                        print(Menu.get_size() + 'Ошибка: введенно не существующие значение')
+                case '4':
+                    data = input(Menu.get_size() + '\nВыберите жанр для фильтрации: ')
+                    if Repository.exists('Styles', 'style', data):
+                        attr = 'style'
+                        value = Repository.get_id('Styles', 'style', data)
+                        break
+                    else:
+                        os.system('clear')
+                        print(Menu.get_size() + 'Ошибка: введенно не существующие значение')
+                case '5':
+                    data = input(Menu.get_size() + '\nВыберите год релиза для фильтрации: ')
+                    if Repository.exists('Releases', 'year', data):
+                        attr = 'release'
+                        value = Repository.get_id('Releases', 'year', data)
+                        break
+                    else:
+                        os.system('clear')
+                        print(Menu.get_size() + 'Ошибка: введенно не существующие значение')
+                case _:
+                    os.system('clear')
+                    print(Menu.get_size() + 'Ошибка: введенно не корректное значение')
+        os.system('clear')
+        print(Menu.get_size() + '\nВыборка из БД\n' + Menu.get_size())
+        Repository.search(attr, value)
 
     def create() -> None:
         os.system('clear')
@@ -93,6 +153,76 @@ class Menu:
                     break
                 case _: Menu.err_input()
         Repository.create(table, obj)
+
+    def update() -> None:
+        os.system('clear')
+        while True:
+            print(Menu.get_size() + f'Обновление объекта из таблицы\n' + Menu.get_size())
+            Repository.search('album', '%')
+            print(f'\n\n0) Вернуться к прошлому меню\n' + Menu.get_size())
+            album = input('Укажите наименование альбома: ')
+            match album:
+                case '0': 
+                    os.system('clear')
+                    return
+                case _:
+                    os.system('clear')
+                    if Repository.exists('Albums', 'album', album):
+                        Menu.update_obj(album)
+                    else: 
+                        print(Menu.get_size() + 'Ошибка: введенно не корректное значение')
+
+    def update_obj(name) -> None:
+        os.system('clear')
+        id = Repository.get_id('Albums', 'album', name)
+        obj = Repository.get_album(id)
+        while True:
+            menu = (
+                Menu.get_size()
+                + f'Обновление данных объекта\n'
+                + Menu.get_size()
+                + f'\n1) Изменить название альбома \t[текущее название: {obj.album}]'
+                + '\n2) Изменить исполнителя \t[текущий исполнитель: '
+                + f'{Repository.get_name("Musicians", "musician", obj.musician)}]'
+                + '\n3) Изменить жанр \t\t[текущий жанр: '
+                + f'{Repository.get_name("Styles", "style", obj.style)}]'
+                + '\n4) Изменить год релиза \t\t[текущий год релиза: '
+                + f'{Repository.get_name("Releases", "year", obj.release)}]'
+                + f'\n5) Записать изменения в базу данных'
+                + f'\n0) Вернуться к прошлому меню\n'
+                + Menu.get_size()
+            )
+            match input(f'{menu}\nВыберите действие: '):
+                case '0': 
+                    os.system('clear')
+                    return
+                case '1':
+                    new_album = input('Укажите новое название: ')
+                    if not Repository.exists('Albums', 'album', new_album):
+                        obj.album = new_album
+                        os.system('clear')
+                    else:
+                        os.system('clear')
+                        print(Menu.get_size() + 'Ошибка: альбом с таким именем уже существует')
+                case '2':
+                    new_musician = input('Укажите нового исполнителя: ')
+                    obj.musician = Repository.get_id('Musicians', 'musician', new_musician)
+                    os.system('clear')
+                case '3':
+                    new_style = input('Укажите новый жанр: ')
+                    obj.style = Repository.get_id('Styles', 'style', new_style)
+                    os.system('clear')
+                case '4':
+                    new_release = input('Укажите новый год релиза: ')
+                    obj.release = Repository.get_id('Releases', 'year', new_release)
+                    os.system('clear')
+                case '5':
+                    Repository.update(id, obj)
+                    os.system('clear')
+                    return
+                case _:
+                    os.system('clear')
+                    print(Menu.get_size() + 'Ошибка: введенно не корректное значение')
 
     def delete() -> None:
         os.system('clear')
